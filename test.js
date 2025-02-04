e3b9acc560c494c6d2c158375534afb5f57cefe9
AbafiM23 <AbafiM23@Z00T6-01>
2025-02-04 10:21:26 +0100

    a1

--- test.js
+++ test.js
@@ -1,17 +1,19 @@
+/*
 function najstSlovo (text, znak) {
     var i = 0
     for(var i=0;i < text.length;i++){
         
-        if(text===znak) console.log("Znak"+znak + "sa nenasiel na pozicii -"+ i)
+        if(text[i]=== znak) 
+        return("Znak"+ znak + "sa nasiel na pozicii -" + i);
 
         
     }
     
-    
+    return "znak sa nenasiel";
 
-              return "znak sa nenasiel"; 
+            
 }
-
+    console.log(najstSlovo("jaro" , "r"))
 
 function poZadu(slovo){
     var slovoOdzadu = "";
@@ -22,3 +24,54 @@ function poZadu(slovo){
 }
 console.log(poZadu("miro"))
 
+function vyskyt(text ,znak){
+var o = 0;
+for(var i = 0;i< text.length;i++){
+if(text[i]===znak){
+o++;
+}
+
+}    
+return o;
+}
+console.log(vyskyt("jojo"))
+
+function factorial (cislo){
+    var vypocet = 1;
+    for ( var i= cislo-1; i<= cislo; i++){
+        vypocet = cislo * i;
+    }
+    return vypocet
+}
+console.log(factorial(5))
+
+function postupnost(x){
+let a = 0; b=1;
+console.log("")
+for(var i = 0; b <= cislo; i++){
+    var newA = b;
+    b= a+b;
+    console.log(b);
+    a= newA
+}
+}
+
+
+async function countdown(odpocitaj){
+    while (odpocitaj > 0){
+        console.log(odpocitaj--);
+        await delay(1000)
+
+    }
+}
+countdown(10);
+*/
+function guessNumber(cislo){
+var userInput = 0;
+while(userInput !== cislo){
+    userInput = parseInt(prompt("Zadajte cislo:"))
+    if(userInput > cislo) console.log()
+}
+}
+
+guessNumber(10);
